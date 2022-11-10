97f738db3682dd9af6ef5faa219503843179ef04
endsinger <102259704+endsinger@users.noreply.github.com>
2022-11-10 19:31:35 +0400

    Add files via upload

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo(@"C:\\");
+                if (dirInfo.Exists)
+                {
+                    Console.WriteLine(dirInfo.GetDirectories().Length + dirInfo.GetFiles().Length);
+                }
+
+                DirectoryInfo newDirectory = new DirectoryInfo(@"/newDirectory");
+                if (!newDirectory.Exists)
+                    newDirectory.Create();
+
+                Console.WriteLine(dirInfo.GetDirectories().Length + dirInfo.GetFiles().Length);
+
+                dirInfo.Delete(true); 
+                Console.WriteLine("Каталог удален");
+
+                DirectoryInfo dirInfo2 = new DirectoryInfo(@"/Users/luft/Desktop/testFolder");
+                string trashPath = "/Users/luft/.Trash/testFolder";
+
+                dirInfo.MoveTo(trashPath);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
